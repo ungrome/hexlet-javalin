0e46abed10c288b30879821e34fb8f52e0f289df
ungrome <ungarov.roman@gmail.com>
2024-08-17 23:19:22 +0300

    fix App, add queryParam of http-session

--- app/src/main/java/org/example/hexlet/HelloWorld.java
+++ app/src/main/java/org/example/hexlet/HelloWorld.java
@@ -4,11 +4,19 @@ import io.javalin.Javalin;
 public class HelloWorld {
 
     public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+    public static Javalin getApp() {
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
         });
+        app.get("/hello", ctx -> {
+            String name = ctx.queryParamAsClass("name", String.class)
+                    .getOrDefault("World!");
+            ctx.result("Hello, " + name + "!");
+        });
 
-        app.get("/", ctx -> ctx.result("Hello World"));
-        app.start(7070);
+        return app;
     }
 }
